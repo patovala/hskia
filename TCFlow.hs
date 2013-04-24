674992d31ea010554d42d32ab46952dce2b69dfd
Patricio Valarezo <patovala@pupilabox.net.ec>
2013-04-25 09:47:19 +1000

    segundo if then else, but i'm afraid that this is wrong approach

--- TCFlow.hs
+++ TCFlow.hs
@@ -30,11 +30,13 @@ data CFGNode
 ----------------------------------------------------------------
 -- PV Given an Expresion return the list of nodes
 --
---
+-- n is a node counter...
 ----------------------------------------------------------------
-eval :: [Stmt] -> [CFGNode]
-eval [] = [] 
-eval ((Asg v e):xs) = (AsgNode v e) : eval xs
+eval :: [Stmt] -> Int -> [CFGNode]
+eval [] n = [] 
+eval ((Asg v e):xs) n = (AsgNode v e) : eval xs (n + 1)
+eval ((If e es):xs) n = (IfGotoNode e (n + 1)) : eval xs (n + 2)
+eval ((Ite e es1 es2):xs) n = (IfGotoNode e (n + 1)): (GotoNode (n + 2)) : eval xs (n + 3)
 
 -- = Asg String Exp
 -- | If Exp [Stmt]
@@ -49,4 +51,6 @@ eval ((Asg v e):xs) = (AsgNode v e) : eval xs
 --
 ----------------------------------------------------------------
 doit s = show val 
-    where val = eval (tParse s)
+    where val = eval (tParse s) 0
+
+doto s = eval (tParse s) 0
