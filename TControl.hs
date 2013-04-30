5204b2d1e630e525b037978161928ae30481cf1f
Patricio Valarezo <patovala@pupilabox.net.ec>
2013-04-30 11:05:57 +1000

    Creating new functions for constants getconst and getconst' are functions for get the constants in a program

--- TControl.hs
+++ TControl.hs
@@ -3,6 +3,7 @@ where
 
 import TCFlow(evalwrpr, eval, CFGNode(..), pprint)
 import TParse (tParse)
+import Syntax (Exp(..))
 
 {-----------------------------------------------------------------------------
 
@@ -24,7 +25,7 @@ data CtrPoint =
 
 type SCFGNode = (Int, CFGNode) -- an improved CFGNode with id
 {-- Test Data --}
-myprogram = "n=0;while(100>n){n=n-1;}"
+myprogram = "n=0;while((100-97)>n){n=n-1*99;}"
 productions' = eval (tParse myprogram) 1  -- 1 cause entry 
 productions = evalwrpr productions'        -- Node has not 
 -- PV use this as a test for feeding later
@@ -36,26 +37,26 @@ sproductions = putids productions 0
 -- extCtrPoint x (_:y:[]) = [] -- get the before last node
 -- extCtrPoint x (y:x:xs) = 
 
-getpred :: CFGNode -> Pos -> [CFGNode] -> Predecesors
-getpred node idn [] = []
-getpred node idn (y@(AsgNode _ _):x:xs) 
-        | node == x = y:getpred node idn xs
-getpred node idn (y@(GotoNode to):xs) 
-        | idn == to = y:getpred node idn xs
-        | otherwise = getpred node idn xs
-getpred node idn (x:xs) = getpred node idn xs
-
-extCtrPoint :: [CFGNode] -> Int -> [CtrPoint] 
-extCtrPoint [] n = []
-extCtrPoint ((GotoNode _):xs) n = extCtrPoint xs (n+1)
-extCtrPoint (x:xs) n = (CtrPoint n x points):(extCtrPoint xs (n+1))
-        where points = getpred x n productions 
-
-showctrp :: [CtrPoint] -> IO()
-showctrp [] = do putStrLn "" 
-showctrp ((CtrPoint n node ps):xs) = do 
-            putStrLn $ show n ++ ": " ++ show node ++ " -> " ++ show ps 
-            showctrp xs 
+--getpred :: CFGNode -> Pos -> [CFGNode] -> Predecesors
+--getpred node idn [] = []
+--getpred node idn (y@(AsgNode _ _):x:xs) 
+--        | node == x = y:getpred node idn xs
+--getpred node idn (y@(GotoNode to):xs) 
+--        | idn == to = y:getpred node idn xs
+--        | otherwise = getpred node idn xs
+--getpred node idn (x:xs) = getpred node idn xs
+--
+--extCtrPoint :: [CFGNode] -> Int -> [CtrPoint] 
+--extCtrPoint [] n = []
+--extCtrPoint ((GotoNode _):xs) n = extCtrPoint xs (n+1)
+--extCtrPoint (x:xs) n = (CtrPoint n x points):(extCtrPoint xs (n+1))
+--        where points = getpred x n productions 
+--
+--showctrp :: [CtrPoint] -> IO()
+--showctrp [] = do putStrLn "" 
+--showctrp ((CtrPoint n node ps):xs) = do 
+--            putStrLn $ show n ++ ": " ++ show node ++ " -> " ++ show ps 
+--            showctrp xs 
 -------------------------------------------------------------------------------
 -- New approach gettin only a tuple with ids
 -- PV
@@ -111,3 +112,25 @@ getgotopred x (y:ys) = getgotopred x ys
 -------------------------------------------------------------------------------
 -- End new approach
 -------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- PV Get the constants from a production
+-- A constant should be get from the expresions
+-------------------------------------------------------------------------------
+getconst :: [SCFGNode] -> [(CFGNode, [Pos])]
+getconst [] = []
+getconst (x@(_, AsgNode _ exp):xs) = (cfgnode, cons) : getconst xs 
+        where cons = filtercons exp 
+              (_, cfgnode) = x
+getconst (x@(_, IfGotoNode exp _):xs) = (cfgnode, cons) : getconst xs 
+        where cons = filtercons exp 
+              (_, cfgnode) = x
+getconst (x:xs) = getconst xs
+
+filtercons :: Exp -> [Int] 
+filtercons (Con i) = [i]
+filtercons (Op _ e1 e2) = filtercons e1 ++ filtercons e2
+filtercons x = [] 
+
+-- This is a wrapper for avoid using a more elegant and elaborated SCFGNode
+getconst' cfgnodes = getconst (putids cfgnodes 0)
