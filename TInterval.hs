7ef8a72e52712fcb89cba18a50f0dddc4dc560ec
Patricio Valarezo <patovala@pupilabox.net.ec>
2013-05-20 10:58:54 +1000

    Fixed interval version in master

--- TInterval.hs
+++ TInterval.hs
@@ -104,8 +104,13 @@ instance Num Interval where
     (+) _ Empty = Empty
     -- [a + c, b + d]
     (Interval a  b) + (Interval c d) = Interval (a + c) (b + d)
-    (Interval (Lb a) (Ub b)) - (Interval (Lb c) (Ub d)) =
-    Interval (Lb(a - d)) (Ub (b - c))
+    (Interval (Lb a) (Ub b)) - (Interval (Lb c) (Ub d)) = Interval (Lb(a - d)) (Ub (b - c))
+    (Interval MinInf (Ub b)) - (Interval (Lb c) _) = Interval (MinInf) (Ub (b - c))
+    (Interval (Lb a) PlusInf) - (Interval _ (Ub d)) = Interval (Lb(a - d)) (PlusInf)
+    (Interval (Lb a) (Ub b)) - (Interval MinInf (Ub d)) = Interval (MinInf) (PlusInf)
+    (Interval (Lb a) (Ub b)) - (Interval (Lb c) PlusInf) = Interval (MinInf) (PlusInf)
+    (Interval MinInf PlusInf) - (Interval _ _) = Interval (MinInf) (PlusInf)
+    (Interval _ _) - (Interval MinInf PlusInf) = Interval (MinInf) (PlusInf)
     Empty - _ = Empty 
     _ - Empty = Empty 
     (Interval (Lb a)  (Ub b)) * (Interval (Lb c) (Ub d)) = 
@@ -125,13 +130,11 @@ instance Fractional Interval where
     (/) Empty x = Empty
     (/) x Empty = Empty
 
-
     (Interval (Lb a)  (Ub b)) / (Interval (Lb 0) (Ub _)) = Empty
     (Interval (Lb a)  (Ub b)) / (Interval (Lb _) (Ub 0)) = Empty
     (Interval (Lb a)  (Ub b)) / (Interval (Lb c) (Ub d)) = 
-       Interval 
-       (Lb (minimum [(a `div` c),(a `div` d),(b `div` c),(b `div` d)]))
-       (Ub (maximum [(a `div` c),(a `div` d),(b `div` c),(b `div` d)]))
+                    Interval (Lb (minimum [(a `div` c),(a `div` d),(b `div` c),(b `div` d)]))
+                             (Ub (maximum [(a `div` c),(a `div` d),(b `div` c),(b `div` d)]))
     (Interval MinInf  _) / (Interval _ _) = Interval (MinInf) (PlusInf)
     (Interval _  PlusInf) / (Interval _ _) = Interval (MinInf) (PlusInf)
     (Interval _  _) / (Interval MinInf _) = Interval (MinInf) (PlusInf)
@@ -142,16 +145,16 @@ instance Fractional Interval where
 union :: Interval -> Interval -> Interval
 union Empty x = x
 union x Empty = x
-union (Interval lb1 ub1) (Interval lb2 ub2) = 
-   Interval (min lb1 lb2) (max ub1 ub2)
+union (Interval lb1 ub1) (Interval lb2 ub2) = Interval (min lb1 lb2) (max ub1 ub2)
 
 -- PV this define the intersection of two intervals
 intersec :: Interval -> Interval -> Interval
 intersec Empty x = Empty 
 intersec x Empty = Empty 
-intersec (Interval (Lb a) (Ub b)) (Interval (Lb c) (Ub d)) =
-  (Interval (Lb b') (Ub c'))
-    where (_:b':c':_) = sort([a,b,c,d])
+intersec (Interval (Lb a) (Ub b)) (Interval (Lb c) (Ub d))  
+    | b' >= c'   = (Interval (Lb b') (Ub c'))
+    | otherwise  = Empty 
+        where (_:b':c':_) = sort([a,b,c,d])
         
 intersec (Interval MinInf PlusInf) i = i 
 intersec i (Interval MinInf PlusInf) = i 
@@ -167,66 +170,56 @@ intersec (Interval lb1 ub1) (Interval lb2 ub2)
 -- lb1 == -oo and ub2 == oo ub1 can't be oo since it was previously catched
 intersec (Interval lb1 ub1) (Interval lb2 ub2)
     | lb1 == MinInf && ub2 == PlusInf = r
-                where (Lb x) = lb2
-                      (Ub y) = ub1
-                      r = case (compare x y) of
-                                EQ -> Empty
-                                GT -> Empty 
-                                LT -> (Interval (Lb y) (Ub x))
+        where (Lb x) = lb2
+              (Ub y) = ub1
+              r = case (compare x y) of
+                        EQ -> Empty
+                        GT -> Empty 
+                        LT -> (Interval (Lb y) (Ub x))
 
 -- ub1 == oo and lb2 == -oo ub2 can't be oo since it was previously catched
 intersec (Interval lb1 ub1) (Interval lb2 ub2)
     | lb2 == MinInf && ub1 == PlusInf = r
-                where (Lb x) = lb1
-                      (Ub y) = ub2
-                      r = case (compare x y) of
-                                EQ -> Empty
-                                GT -> Empty
-                                LT -> Interval (Lb x) (Ub y)
+        where (Lb x) = lb1
+              (Ub y) = ub2
+              r = case (compare x y) of
+                        EQ -> Empty
+                        GT -> Empty
+                        LT -> Interval (Lb x) (Ub y)
 -- [-oo,a] [b,c]
 intersec (Interval MinInf ub1) (Interval lb2 ub2) = r
-                where
-                    (Ub x) = ub1
-                    (Lb y) = lb2
-                    (Ub z) = ub2
-                    -- IPV because intervals seems to be inclusive
-                    -- r = if (x<=y && x<=z) then Empty 
-                    --else (Interval (Lb x') (Ub y'))
-                    r = if (x<y && x<z) then Empty 
-                        else (Interval (Lb x') (Ub y'))
-                    (x':y':z':[]) = Data.List.sort (x:y:z:[])
+        where
+            (Ub x) = ub1
+            (Lb y) = lb2
+            (Ub z) = ub2
+            -- IPV because intervals seems to be inclusive
+            -- r = if (x<=y && x<=z) then Empty else (Interval (Lb x') (Ub y'))
+            r = if (x<y && x<z) then Empty else (Interval (Lb x') (Ub y'))
+            (x':y':z':[]) = sort (x:y:z:[])
 -- [a,oo] [b,c]
 intersec (Interval lb1 PlusInf) (Interval lb2 ub2) = r
-                where
-                    (Lb x) = lb1
-                    (Lb y) = lb2
-                    (Ub z) = ub2
-                    r = if (x>y && x>z) then Empty 
-                        else (Interval (Lb y') (Ub z'))
-                    (x':y':z':[]) = Data.List.sort (x:y:z:[])
+        where
+            (Lb x) = lb1
+            (Lb y) = lb2
+            (Ub z) = ub2
+            r = if (x>y && x>z) then Empty else (Interval (Lb y') (Ub z'))
+            (x':y':z':[]) = Data.List.sort (x:y:z:[])
 -- [a,b] [-oo,c]
 intersec (Interval lb1 ub1) (Interval MinInf ub2) = r
-                where
-                    (Lb x) = lb1
-                    (Ub y) = ub1
-                    (Ub z) = ub2
-                    r = if (z<x && z<y) then Empty 
-                        else (Interval (Lb x') (Ub y'))
-                    (x':y':z':[]) = Data.List.sort (x:y:z:[])
+        where
+            (Lb x) = lb1
+            (Ub y) = ub1
+            (Ub z) = ub2
+            r = if (z<x && z<y) then Empty else (Interval (Lb x') (Ub y'))
+            (x':y':z':[]) = sort (x:y:z:[])
 -- [a,b] [c,oo]
 intersec (Interval lb1 ub1) (Interval lb2 PlusInf) = r
-                where
-                    (Lb x) = lb1
-                    (Ub y) = ub1
-                    (Lb z) = lb2
-                    r = if (z>x && z>y) then Empty 
-                        else (Interval (Lb y') (Ub z'))
-                    (x':y':z':[]) = Data.List.sort (x:y:z:[])
-
--- intersec a b = error ("interval error " ++ (show a) ++ (show b))
-
---
---intersec _ _ = Empty
+        where
+            (Lb x) = lb1
+            (Ub y) = ub1
+            (Lb z) = lb2
+            r = if (z>x && z>y) then Empty else (Interval (Lb y') (Ub z'))
+            (x':y':z':[]) = sort (x:y:z:[])
 
 -- Test the interval behaviour
 a = [(MinInf),(Lb (-1)),(Lb 0),(Lb 2),(Lb 4)]
@@ -236,8 +229,7 @@ ivs = [(x,y)| x<-ivs', y<-ivs']
     where ivs' = (map (\(x,y) -> Interval x y) (zip a b)) ++ [ Empty ]
 
 -- Interval Tester, given an interval operations
-interTest::[(Interval, Interval)]->(Interval -> 
-            Interval -> Interval)->[(Interval, Interval, Interval)]
+interTest::[(Interval, Interval)]->(Interval -> Interval -> Interval)->[(Interval, Interval, Interval)]
 interTest [] _ = []
 interTest ((a,b):xs) f = (a, b, c) : interTest xs f
         where c = (f a b)
